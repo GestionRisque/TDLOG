ccd6c9a072d758988092658f00d0e4e5b6fc5567
yoyoyousihan <yousihan@me.com>
2015-12-12 21:07:39 +0100

    Gestion Risque v.0.6

--- SimpleModelling.py
+++ SimpleModelling.py
@@ -2,17 +2,22 @@ __author__ = 'sihanyou'
 
 import GlobalValue
 import arch
-import math
 import statsmodels.api as sm
+import random
+import numpy as np
+import math
+from scipy import stats as st
+from scipy.optimize import minimize
 
 def main():
     GlobalValue.init()
     GlobalValue.modelParams = []
     for actif in GlobalValue.yahooData:
-        print(actif)
+        #print(actif)
         arma = ARMA(actif)
         garch = GARCH(actif)
-        GlobalValue.modelParams.append({'arma':arma,'garch':garch})
+        sv = SV(actif)
+        GlobalValue.modelParams.append({'arma':arma,'garch':garch,'sv':sv})
         #print(priceMvt)
 
 
@@ -23,6 +28,93 @@ def returns(share):
     for i in range(1,n-1):
         r.append(math.log(share[i]/share[i-1]))
     return r
+
+
+def vctPower(v,p):
+    res = []
+    for i in range(0,len(v)-1):
+        if v[i]!=0:
+            res.append(math.log(v[i] ** p))
+    return res
+
+def pseudoData(theta, u, e):
+    a = theta[0]
+    b = theta[1]
+    s = theta[2]
+
+    h = []
+    y = []
+    h.append(e[0] * s ** 2 / (1 - b ** 2) + a / (1-b))
+    y.append(math.exp(h[0]/2)*u[0])
+    for i in range(1,len(e)-1):
+        h.append(a + b * h[i-1]+e[i])
+        y.append(math.exp(h[i]/2)*u[i])
+    return y
+
+
+def distance(mu,nu):
+    return (mu[0]-nu[0])**2 + (mu[1]-nu[1])**2 + (mu[2]-nu[2])**2
+
+def mu(r):
+    ts = vctPower(r,2)
+
+    x = ts[0:-1]
+
+    y = ts[1:]
+
+    mod = st.linregress(x,y)
+
+    nu = []
+    nu.append(mod[0])
+    nu.append(mod[1])
+    nu.append(mod[4])
+
+    return nu
+
+def target(theta):
+
+    p = pseudoData(theta,u,e)
+
+    mus = mu(p)
+
+    return distance(mus,nus)
+
+
+def SV_opt(share,i):
+
+    r = returns(share)
+
+    length = len(r)
+
+    random.seed(i)
+
+    global u
+
+    u = np.random.normal(0, 1, 20*length)
+
+    global e
+
+    e = np.random.normal(0, 1, 20*length)
+
+    global nus
+
+    nus = mu(r)
+
+    bnds = ((-10, 0.999), (-1, 0.999),(0.001,10))
+
+    params = minimize(target,(-5,0.3,0.5),bounds=bnds,method='SLSQP')
+
+    return params.x
+
+
+
+def SV(share):
+
+    params = []
+    for i in range (1,20):
+        params.append(SV_opt(share,i))
+
+    return np.mean(params,axis=0)
 
 
 def ARMA(share):
@@ -43,9 +135,6 @@ def GARCH(share):
     return res.params
 
 
-def SV(share):
-
-    r = returns(share)
 
     return
 
