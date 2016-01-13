e35f86ea5fd3c5bf6af4d6afc0acb71e333c4e82
yoyoyousihan <sihanyou@MacBook-Air-de-Sihan.local>
2016-01-13 10:27:40 +0100

    hh

--- SimpleModelling.py
+++ SimpleModelling.py
@@ -8,6 +8,8 @@ import numpy as np
 import math
 from scipy import stats as st
 from scipy.optimize import minimize
+import warnings
+warnings.filterwarnings("ignore")
 
 def main(processPercentSignal,currentPercent):
     GlobalValue.init()
